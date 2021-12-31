9f04fba90f22538228535734905a10131a19351e
johanattia <attia.johan@gmail.com>
2021-12-31 01:50:59 +0100

    SAINT: introduce input feature schema class

---
+++ structured_transformers/saint/schema.py
@@ -0,0 +1,29 @@
+"""Features schema for structured/tabular models."""
+
+from typing import Dict, Optional, Union
+from pydantic import BaseModel, validator
+
+import tensorflow as tf
+
+
+class FeatureSchema(BaseModel):
+    name: str
+    feature_type: str  # `NUMERICAL` or `CATEGORICAL`
+    feature_dimension: int  # 1 if `NUMERICAL`, N if `CATEGORICAL`
+    field_type: Optional[Union[tf.io.FixedLenFeature, tf.dtypes.DType]] = None
+    description: Optional[str] = None
+    minimum: Optional[Union[int, float]] = None
+    maximum: Optional[Union[int, float]] = None
+
+    @validator("feature_type")
+    def feature_type_match(cls, value: str):
+        if isinstance(value, str) and (
+            value.upper() not in ["NUMERICAL", "CATEGORICAL"]
+        ):
+            raise ValueError("`feature_type` must be in [`NUMERICAL`, `CATEGORICAL`]")
+        return value.upper()
+
+
+class FeaturesMapping(BaseModel):
+    name: str
+    mapping: Dict[str, FeatureSchema]
