77e78d3eac6a1d9157a3e4788c3fe1437945225a
johanattia <attia.johan@gmail.com>
2022-03-17 22:30:59 +0100

    implement contrastive loss, denoising loss and training logic

--- tf_saint/saint.py
+++ tf_saint/saint.py
@@ -3,37 +3,36 @@
 from typing import Callable, Dict, Iterable, Union
 
 import tensorflow as tf
-from tensorflow.python.keras.engine import data_adapter
 
 from .schema import FeatureType, InputFeaturesSchema
 from .layers import MLP, StructuredEmbedding, SAINTBlock
 from .augmentation import CutMix, Mixup
 
 
-class SAINT(tf.keras.Model):
-    """[summary]
+# TODO: explore complete tensor projection instead of for-loops for denoising
 
-    Args:
-        input_schema (InputFeaturesSchema): [description]
-        n_layers (int): [description]
-        num_heads (int): [description]
-        embed_dim (int): [description]
-        hidden_dim (int): [description]
-        dropout (float, optional): [description]. Defaults to 0.1.
-        epsilon (float, optional): [description]. Defaults to 1e-6.
-        cutmix_probability (float, optional): [description]. Defaults to 0.5.
-        mixup_alpha (float, optional): [description]. Defaults to 0.5.
-        seed (int, optional): [description]. Defaults to 26.
-        embeddings_initializer (str, optional): [description]. Defaults to "uniform".
-        kernel_initializer (Union[str, Callable], optional): [description]. Defaults to "glorot_uniform".
-        bias_initializer (Union[str, Callable], optional): [description]. Defaults to "zeros".
-        embeddings_regularizer (Union[str, Callable], optional): [description]. Defaults to None.
-        kernel_regularizer (Union[str, Callable], optional): [description]. Defaults to None.
-        bias_regularizer (Union[str, Callable], optional): [description]. Defaults to None.
-        activity_regularizer (Union[str, Callable], optional): [description]. Defaults to None.
-        embeddings_constraint (Union[str, Callable], optional): [description]. Defaults to None.
-        kernel_constraint (Union[str, Callable], optional): [description]. Defaults to None.
-        bias_constraint (Union[str, Callable], optional): [description]. Defaults to None.
+
+class SAINT(tf.keras.Model):
+    """Self-Attention and Intersample Attention Transformer (SAINT).
+    The training logic implemented is a self-supervised pre-training technique composed
+    of a constrastive loss and tabular features reconstruction.
+
+    For more details, see the paper: https://arxiv.org/pdf/2106.01342.pdf.
+
+    Example:
+    ```python
+    import tensorflow as tf
+    import tf_saint
+
+    dataset_schema = tf_saint.input_schema_from_json(dataset_schema_json)
+    tabular_model = tf_saint.SAINT(
+        input_schema=dataset_schema,
+        n_layers=6,
+        num_heads=8
+        embed_dim=512,
+        hidden_dim512,
+    )
+    ```
     """
 
     def __init__(
@@ -60,6 +59,30 @@ class SAINT(tf.keras.Model):
         bias_constraint: Union[str, Callable] = None,
         **kwargs,
     ):
+        """Model initilization.
+
+        Args:
+            input_schema (InputFeaturesSchema): _description_
+            n_layers (int): _description_
+            num_heads (int): _description_
+            embed_dim (int): _description_
+            hidden_dim (int): _description_
+            dropout (float, optional): _description_. Defaults to 0.1.
+            epsilon (float, optional): _description_. Defaults to 1e-6.
+            cutmix_probability (float, optional): _description_. Defaults to 0.5.
+            mixup_alpha (float, optional): _description_. Defaults to 0.5.
+            seed (int, optional): _description_. Defaults to 26.
+            embeddings_initializer (str, optional): _description_. Defaults to "uniform".
+            kernel_initializer (Union[str, Callable], optional): _description_. Defaults to "glorot_uniform".
+            bias_initializer (Union[str, Callable], optional): _description_. Defaults to "zeros".
+            embeddings_regularizer (Union[str, Callable], optional): _description_. Defaults to None.
+            kernel_regularizer (Union[str, Callable], optional): _description_. Defaults to None.
+            bias_regularizer (Union[str, Callable], optional): _description_. Defaults to None.
+            activity_regularizer (Union[str, Callable], optional): _description_. Defaults to None.
+            embeddings_constraint (Union[str, Callable], optional): _description_. Defaults to None.
+            kernel_constraint (Union[str, Callable], optional): _description_. Defaults to None.
+            bias_constraint (Union[str, Callable], optional): _description_. Defaults to None.
+        """
         super().__init__(**kwargs)
 
         # Input schema
@@ -95,6 +118,11 @@ class SAINT(tf.keras.Model):
         self.bias_constraint = bias_constraint
 
     def build(self, input_shape: Union[tf.TensorShape, Iterable[tf.TensorShape]]):
+        """_summary_
+
+        Args:
+            input_shape (Union[tf.TensorShape, Iterable[tf.TensorShape]]): _description_
+        """
         # CLS token
         self._CLS = self.add_weight(
             name="CLS",
@@ -212,6 +240,16 @@ class SAINT(tf.keras.Model):
         training: bool,
         augmentation: bool = False,
     ) -> tf.Tensor:
+        """_summary_
+
+        Args:
+            inputs (Union[tf.Tensor, Dict[str, tf.Tensor]]): _description_
+            training (bool): _description_
+            augmentation (bool, optional): _description_. Defaults to False.
+
+        Returns:
+            tf.Tensor: _description_
+        """
         # Structured Embedding layer
         if augmentation:
             augmented_inputs = self.cutmix(inputs)
@@ -233,67 +271,137 @@ class SAINT(tf.keras.Model):
         batch_output = {
             "output": contextual_output,
             "flatten_output": self.flatten(contextual_output),
-            "cls_output": contextual_output[:, 0, :]
+            "cls_output": contextual_output[:, 0, :],
         }
-
         return batch_output
 
     def compile(
         self,
+        denoising_lambda: float,
         contrastive_temperature: float,
-        contrastive_optimizer: tf.keras.optimizers.Optimizer,
-        reconstruction_optimizer: tf.keras.optimizers.Optimizer,
-        **kwargs
+        **kwargs,
     ):
-        """
-        
+        """_summary_
+
+        Args:
+            denoising_lambda (float): _description_
+            contrastive_temperature (float): _description_
         """
         super().compile(**kwargs)
 
+        self.denoising_lambda = denoising_lambda
         self.contrastive_temperature = contrastive_temperature
-        self.contrastive_optimizer = contrastive_optimizer
-        self.reconstruction_optimizer = reconstruction_optimizer
-        
-        self.contrastive_loss_tracker = tf.keras.metrics.Mean(name="contrastive_loss_tracker")
-        self.contrastive_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-            name="contrastive_accuracy"
-        )
 
-        self.reconstruction_losses = {}
+        self.contrastive_loss_tracker = tf.keras.metrics.Mean(
+            name="contrastive_loss_tracker"
+        )
+        self.denoising_tracker = {
+            feature.name: tf.keras.metrics.Mean(name=f"{feature.name}_tracker")
+            for feature in self.input_schema.ordered_features
+        }
 
     def train_step(self, data: Union[tf.Tensor, Dict[str, tf.Tensor]]):
-        # Processing data before forward
-        x, y, sample_weight = tf.keras.utils.unpack_x_y_sample_weight(data)
+        """Training logic with two losses to optimize:
+        - InfoNCE loss for contrastive learning, including data augmentation
+        - Denoising loss for features reconstruction
 
-        # Contrastive learning
+        Args:
+            data (Union[tf.Tensor, Dict[str, tf.Tensor]]): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        # Processing data before forward propagation
+        x, _, _ = tf.keras.utils.unpack_x_y_sample_weight(data)
+
         with tf.GradientTape() as tape:
-            features = self(x, training=True)
-            augmented_features = self(x, training=True, augmentation=True)
+            # Forward
+            features1 = self(x, training=True)
+            features1 = features1["flatten_output"]
 
-            flatten_output = features["flatten_output"]
-            augmented_flatten_output = augmented_features["flatten_output"]
+            features2 = self(x, training=True, augmentation=True)
+            features2 = features2["flatten_output"]
 
-        # Contrastive learning
+            # Contrastive loss
+            projection1 = self.projection_head1(features1)
+            projection2 = self.projection_head2(features2)
 
+            contrastive_loss = self.contrastive_loss(
+                projection1=projection1, projection2=projection2
+            )
+
+            # Denoising losses
+            denoising_outputs = {
+                feature.name: getattr(self, f"{feature.name}_denoising")(features2)
+                for feature in self.input_schema.ordered_features
+            }
+            denoising_loss = self.denoising_loss(inputs=x, outputs=denoising_outputs)
+
+            # Pre-training loss
+            loss = contrastive_loss + self.denoising_lambda * denoising_loss
+
+        self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
         return NotImplemented
 
-    def contrastive_loss(self, features1: tf.Tensor, features2: tf.Tensor) -> tf.Tensor:
+    def contrastive_loss(
+        self, projection1: tf.Tensor, projection2: tf.Tensor
+    ) -> tf.Tensor:
+        """Contrastive loss implementation from https://keras.io/examples/vision/semisupervised_simclr/.
 
-        normalized_features1 = tf.math.l2_normalize(projections_1, axis=1)
-        normalized_features2 = tf.math.l2_normalize(projections_2, axis=1)
+        Args:
+            projection1 (tf.Tensor): _description_
+            projection2 (tf.Tensor): _description_
+
+        Returns:
+            tf.Tensor: _description_
+        """
+        normalized_projection1 = tf.math.l2_normalize(projection1, axis=1)
+        normalized_projection2 = tf.math.l2_normalize(projection2, axis=1)
 
         similarities = (
-            tf.matmul(
-                normalized_features1,
-                normalized_features2,
-                transpose_b=True
-            ) / self.contrastive_temperature
+            tf.matmul(normalized_projection1, normalized_projection2, transpose_b=True)
+            / self.contrastive_temperature
         )
 
-        batch_size = tf.shape(features1)[0]
+        batch_size = tf.shape(projection1)[0]
         contrastive_labels = tf.range(batch_size)
-        
-        return
+
+        loss_1_2 = tf.keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, similarities, from_logits=True
+        )
+        loss_2_1 = tf.keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, tf.transpose(similarities), from_logits=True
+        )
+        return (loss_1_2 + loss_2_1) / 2
+
+    def denoising_loss(
+        self, inputs: Dict[str, tf.Tensor], outputs: Dict[str, tf.Tensor]
+    ) -> tf.Tensor:
+        """_summary_
+
+        Args:
+            inputs (Dict[str, tf.Tensor]): _description_
+            outputs (Dict[str, tf.Tensor]): _description_
+
+        Returns:
+            tf.Tensor: _description_
+        """
+        loss = 0
+        for feature in self.input_schema.ordered_features:
+
+            if feature.feature_type is FeatureType.CATEGORICAL:
+                feature_loss = tf.keras.losses.sparse_categorical_crossentropy(
+                    inputs[feature.name], outputs[feature.name], from_logits=True
+                )
+            else:
+                feature_loss = tf.keras.losses.mean_squared_error(
+                    inputs[feature.name], outputs[feature.name]
+                )
+
+            self.denoising_tracker[feature.name].update_state(feature_loss)
+            loss += feature_loss
+
+        return loss
 
     def get_config(self):
         raise NotImplementedError("Not yet implemented")
