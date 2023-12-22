1cc9de33bb8cf797028d1f7efc54fb94a3649dc2
johanattia <attia.johan@gmail.com>
2023-12-22 14:24:41 +0100

    initiate torch distributed runner

--- src/recommendation_transformer/torch/runner/__init__.py
+++ src/recommendation_transformer/torch/runner/__init__.py
@@ -1,3 +1,6 @@
 from recommendation_transformer.torch.runner.accelerate_runner import AccelerateRunner
 from recommendation_transformer.torch.runner.deepspeed_runner import DeepSpeedRunner
+from recommendation_transformer.torch.runner.distributed_runner import (
+    TorchDistributedRunner,
+)
 from recommendation_transformer.torch.runner.torch_runner import TorchRunner
